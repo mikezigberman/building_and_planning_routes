c2b4282e1a75f621dc904f5ba86522967115f12a
Mike Zigberman <mikezigberman@yahoo.com>
2023-02-15 14:57:05 +0100

    find routes(cities app): adding the CreateView function

--- src/cities/urls.py
+++ src/cities/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from cities.views import *
 
+from cities.views import CityCreateView
+
 urlpatterns = [
     path('', home, name='home'),
-    path('detail/<int:pk>/', CityDetailView.as_view(), name='detail')
+    path('detail/<int:pk>/', CityDetailView.as_view(), name='detail'),
+    path('add/', CityCreateView.as_view(), name='create'),
 ]
--- src/cities/views.py
+++ src/cities/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.views.generic import DetailView, CreateView
+from django.urls import reverse_lazy
 
 from cities.models import City
 
 __all__ = (
-    'home', 'CityDetailView'
+    'home', 'CityDetailView', 'CityCreateView',
 )
 
 from cities.forms import HtmlForm, CityForm
@@ -29,3 +30,10 @@ def home(request, pk=None):
 class CityDetailView(DetailView):
     queryset = City.objects.all()
     template_name = 'cities/detail.html'
+
+class CityCreateView(CreateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/create.html'
+    success_url = reverse_lazy('cities:home')
+
