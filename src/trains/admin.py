3612981a2cf09ca713f13fb7a85be3bbafd1152f
Mike Zigberman <mikezigberman@yahoo.com>
2023-02-16 16:19:21 +0100

    find routes(train app): improved display of trains in the admin panel

--- src/trains/admin.py
+++ src/trains/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from trains.models import Train
+
+class TrainAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Train
+    list_display = ('name', 'from_city', 'to_city', 'travel_time')
+    list_editable = ('travel_time', ) # 'from_city'
+
+admin.site.register(Train, TrainAdmin)
--- src/trains/models.py
+++ src/trains/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 from cities.models import City
 
@@ -21,6 +22,21 @@ class Train(models.Model):
             verbose_name = 'Train'
             verbose_name_plural = 'Trains'
             ordering = ['travel_time']
+
+        def clean(self):
+            if self.from_city == self.to_city:
+                raise ValidationError('Изменить город прибытия')
+            qs = Train.objects.filter(
+                from_city=self.from_city, to_city=self.to_city,
+                travel_time=self.travel_time).exclude(pk=self.pk)
+            if qs.exists():
+                raise ValidationError('Измените время в пути')
+
+        def save(self, *args, **kwargs):
+            self.clean()
+            super().save(*args, **kwargs)
+
+
 
 class TrainTest(models.Model):
     name = models.CharField(max_length=50, unique=True,
