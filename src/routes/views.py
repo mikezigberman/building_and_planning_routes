97e52854f2611b9a59aa1b5fc87e7fd7d4faa158
Mike Zigberman <mikezigberman@yahoo.com>
2023-02-16 19:45:30 +0100

    find routes(app routes): route search data processing function

--- src/routes/views.py
+++ src/routes/views.py
@@ -1,6 +1,16 @@
+from django.contrib import messages
 from django.shortcuts import render
 from routes.forms import RouteForm
 
 def home(request):
     form = RouteForm()
     return render(request, 'routes/home.html', {'form': form})
+
+def find_routes(request):
+    if request.method == "POST":
+        form = RouteForm(request.POST)
+        return render(request, 'routes/home.html', {'form': form})
+    else:
+        form = RouteForm()
+        messages.error(request, "No data to search")
+        return render(request, 'routes/home.html', {'form': form})
