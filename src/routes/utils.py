41ffaac5090ed5c2bb89aa20a987e10e68ff7e20
Mike Zigberman <mikezigberman@yahoo.com>
2023-02-17 20:51:22 +0100

    find routes(app routes): search for routes in which the time is less than the user-specified

--- src/routes/utils.py
+++ src/routes/utils.py
@@ -38,4 +38,25 @@ def get_routes(request, form) -> dict:
                 right_ways.append(route)
         if not right_ways:
             raise ValueError('Route through these cities is not possible')
+    else:
+        right_ways = all_ways
+    trains = []
+    all_trains = {}
+    for q in qs:
+        all_trains.setdefault((q.from_city_id, q.to_city_id), [])
+        all_trains[(q.from_city_id, q.to_city_id)].append(q)
+    for route in right_ways:
+        tmp = {}
+        tmp['trains'] = []
+        total_time = 0
+        for i in range(len(route) - 1):
+            qs = all_trains[(route[i], route[i + 1])]
+            q = qs[0]
+            total_time += q.travel_time
+            tmp['trains'].append(qs)
+        tmp['total_time'] = total_time
+        if total_time <= travelling_time:
+            trains.append(tmp)
+    if not trains:
+        raise ValueError('Travel time is longer than specified')
     return context
