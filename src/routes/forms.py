b3a9d85ff34bba87891e9527df6435d3d2091615
Mike Zigberman <mikezigberman@yahoo.com>
2023-02-17 22:53:27 +0100

    find routes(app routes): form for saving routes

--- src/routes/forms.py
+++ src/routes/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from cities.models import City
+from trains.models import City, Train
+from routes.models import Route
+
 
 class RouteForm(forms.Form):
     from_city = forms.ModelChoiceField(label='Where from:', queryset=City.objects.all(), widget=forms.Select(
@@ -22,3 +24,30 @@ class RouteForm(forms.Form):
                                          'class': 'form-control',
                                          'placeholder': 'Travel time'
     }))
+
+class RouteModelForm(forms.ModelForm):
+    name = forms.CharField(label='Route name', widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Enter route name'
+    }))
+    from_city = forms.ModelChoiceField(queryset=City.objects.all(), widget=forms.HiddenInput()
+    )
+
+    to_city = forms.ModelChoiceField(queryset=City.objects.all(), widget=forms.HiddenInput()
+
+    )
+
+    trains = forms.ModelMultipleChoiceField(
+        queryset=Train.objects.all(),
+        required=False, widget=forms.SelectMultiple(
+            attrs={'class': 'form-control d-none'}
+        )
+    )
+
+    travel_times = forms.IntegerField(widget=forms.HiddenInput()
+
+    )
+
+    class Meta:
+        model = Route
+        fields = '__all__'
