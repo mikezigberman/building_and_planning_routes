e81f7147092bc44bfa14ff2105c7768117e58f74
Mike Zigberman <mikezigberman@yahoo.com>
2023-02-18 15:39:55 +0100

    find routes(authentication system): creating an accounts app and creating a user login form

---
+++ src/accounts/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth.hashers import check_password
+
+User = get_user_model()
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField(label='username', widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Enter username'
+    }))
+    password = forms.CharField(label='username', widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Enter password'
+    }))
+
+    def clean(self, *args, **kwargs):
+            username = self.cleaned_data.get('username')
+            password = self.cleaned_data.get('password')
+            if username and password:
+                qs = User.objects.filter(username=username)
+                if not qs.exists():
+                    raise forms.ValueError('This user does not exist')
+                if not check_password(password, qs[0].password):
+                    raise forms.ValueError('Incorrect password')
+                user = authenticate(username=username, password=password)
+                if not user:
+                    raise forms.ValueError('This user is not active')
+            return super().clean(*args, **kwargs)
