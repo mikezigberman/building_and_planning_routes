aab7550e08d733af80b56b8208e04e0a233ef238
Mike Zigberman <mikezigberman@yahoo.com>
2023-02-19 19:32:25 +0100

    find routes(testing): testing the function of building a graph and finding a route

--- src/routes/tests.py
+++ src/routes/tests.py
@@ -6,6 +6,8 @@ from trains.models import Train
 
 from routes import views as routes_view
 from cities import views as cities_view
+from routes.forms import RouteForm
+from routes.utils import dfs_paths, get_graph
 
 class AllTestsCase(TestCase):
     def setUp(self) -> None:
@@ -74,3 +76,10 @@ class AllTestsCase(TestCase):
         self.assertTemplateUsed(response, template_name='cities/detail.html')
         self.assertEqual(response.resolver_match.func.__name__,
                          cities_view.CityDetailView.as_view().__name__)
+
+    def test_find_all_routes(self):
+        qs = Train.objects.all()
+        graph = get_graph(qs)
+        all_routes = list(dfs_paths(graph, self.city_A.id, self.city_E.id))
+        self.assertEqual(len(all_routes), 4)
+
